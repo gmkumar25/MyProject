90193d4975d623e8530785eeeff80106fa3b905d
gmkumar25 <107688719+gmkumar25@users.noreply.github.com>
2022-07-22 18:08:53 +0530

    Add files via upload

---
+++ src/test/java/StepDefinitions/FailedRerun.java
@@ -0,0 +1,33 @@
+package StepDefinitions;
+
+/**
+ * This File - Similar to FailedTestNG.xml, will pick the failed test scenarios
+ * from the mentioned file and rerun them.
+ * 
+ */
+
+import org.testng.annotations.DataProvider;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        plugin = {"pretty",
+                "html:target/cucumber-reports/TestReport.html",
+                "json:target/cucumber-reports/TestReport.json",
+                "rerun:target/failedscenariosrerun.txt"
+                              
+        },
+        features = {"@target/failedscenariosrerun.txt"},
+        glue = {"StepDefinitions"},
+        tags= "@smoke"
+        
+)
+public class FailedRerun extends AbstractTestNGCucumberTests {
+	
+	@Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
+}
